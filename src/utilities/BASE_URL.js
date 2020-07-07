3f32b459f06552ef798f720c9da8d01197dd76e7
Arthur Song <arth3rs0ng@gmail.com>
2020-07-07 15:22:24 -0700

    Changed websocket url to wss

--- src/utilities/BASE_URL.js
+++ src/utilities/BASE_URL.js
@@ -1,3 +1,5 @@
 export const BASE_URL = `https://console-poker-api.herokuapp.com`
+// export const BASE_URL = `http://localhost:3001`
 
-export const WS_URL = `ws://console-poker-api.herokuapp.com`
+export const WS_URL = `wss://console-poker-api.herokuapp.com`
+// export const WS_URL = `ws://localhost:3001`
