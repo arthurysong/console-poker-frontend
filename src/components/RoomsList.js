ce26d864bbbf94bbf7a3f852883b1d48f7081ae8
Arthur Song <arth3rs0ng@gmail.com>
2020-07-08 12:44:36 -0700

    Added Header for RoomsList Component

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -77,7 +77,9 @@ class RoomsList extends React.Component {
                 <button className="nes-btn smaller-btn is-error" id="test" onClick={this.clickHandler}>Log Out</button>&nbsp;
                 <button className="nes-btn is-success smaller-btn" onClick={this.redirectToDeposits}>Deposit</button><br/><br/><br/><br/>
                 {/* <div className="ne"></div> */}
-                <NavLink to="/rooms/new" className="nes-btn is-primary smaller-btn">Create Room</NavLink><br/><br/>
+                
+                <h1>Join a Room!</h1>
+                <NavLink to="/rooms/new" className="nes-btn is-primary smaller-btn">Create Room</NavLink>
                 <ul id="rooms_ul">
                     {this.renderRooms()}
                 </ul>
