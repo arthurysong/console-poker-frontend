e02abc201ff38f87bfc75ea538bfddb421532d9c
Arthur Song <arth3rs0ng@gmail.com>
2020-07-11 15:24:31 -0700

    Using same CLIENT_ID for production because we are using test keys not live

--- src/utilities/BASE_URL.js
+++ src/utilities/BASE_URL.js
@@ -6,5 +6,5 @@ export const WS_URL = process.env.NODE_ENV === `development` ? `ws://localhost:3
 
 export const DOMAIN = process.env.NODE_ENV === `development` ? `http://localhost:3000` : `https://console-poker.herokuapp.com`
 
-export const CLIENT_ID = process.env.NODE_ENV === `development` ? `ca_HchUekElUVgyyKljmwfv4DjBMT7NEfT3` : `ca_HchUF4wItTEpNVJk3H2ibQ3iGMg6DVfn
-`
+export const CLIENT_ID = `ca_HchUekElUVgyyKljmwfv4DjBMT7NEfT3`
+// export const CLIENT_ID = process.env.NODE_ENV === `development` ? `ca_HchUekElUVgyyKljmwfv4DjBMT7NEfT3` : `ca_HchUF4wItTEpNVJk3H2ibQ3iGMg6DVfn`
