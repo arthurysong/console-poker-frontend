b517208480cb2fb31aa368c9a43c101e72a87635
Arthur Song <arth3rs0ng@gmail.com>
2020-07-29 16:07:19 -0700

    Refactoring Players Component, simple syntax and variable assignment refactoring

--- src/components/Room/NewRoundButton.js
+++ src/components/Room/NewRoundButton.js
@@ -4,18 +4,14 @@ import { useSelector } from 'react-redux';
 
 function NewRoundButton({ round }) {
     const game = useSelector(state => state.game);
-    console.log('in round button');
-    console.log(round);
 
     if (!round || !round.is_playing) {
         return (
-            <>
-                <button 
-                    className={`nes-btn ${game.startable ? 'is-primary' : 'is-disabled'}`} 
-                    onClick={() => startNewRound(game.id)}>
-                    New Round
-                </button>
-            </>
+            <button 
+                className={`nes-btn ${game.startable ? 'is-primary' : 'is-disabled'}`} 
+                onClick={() => startNewRound(game.id)}>
+                New Round
+            </button>
         )
     } 
     return ""
--- src/components/Room/Players.js
+++ src/components/Room/Players.js
@@ -2,33 +2,25 @@ import React from 'react';
 import Player from './Player';
 import { useSelector } from 'react-redux';
 
-function Players (props) {
-    const game = useSelector(state => state.game);
-    // const gamePlayers = useSelector(state => state.gamePlayers);
+function Players ({ images }) {
+    const ordered_users = useSelector(state => state.game.ordered_users);
 
     const renderPlayers = () => {
-        if (game.ordered_users) {
-        // if (gamePlayers) {
-            // console.log('testetestsetsetset');
-            // console.log(game);
-            return (
-                // <div id="players_container" style={{position: "relative"}}>
-                <div id="players_container">
-                {/* <div> */}
-                    {game.ordered_users.map((user,index) => 
-                        <Player 
-                            key={index} 
-                            user={user} 
-                            images={props.images}/>)}
-                </div>
-            )
+        if (ordered_users) {
+            return ordered_users.map((user,index) => 
+                <Player 
+                    key={index} 
+                    user={user} 
+                    images={images}/>)
         }
     }
 
     return(
-        <ul>
-            {renderPlayers()}
-        </ul>
+        <div id="players_container">
+            <ul>
+                {renderPlayers()}
+            </ul>
+        </div>
     )
 }
 
