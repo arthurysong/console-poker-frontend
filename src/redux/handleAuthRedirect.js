668215e3875e47222ad00afb230e13bd07c14832
Arthur Song <arthursong14@gmail.com>
2020-08-05 12:23:23 -0700

    Adding space between Amount and FullName Label Adding more margin between exchange button and input above in deposit

--- src/redux/handleAuthRedirect.js
+++ src/redux/handleAuthRedirect.js
@@ -2,7 +2,7 @@ export default function handleAuthRedirect(isLoggedIn, history){
     let curr_url = window.location.pathname;
     // console.log(isLoggedIn, curr_url);
     if (isLoggedIn && (curr_url === "/" || curr_url === "/login" || curr_url === "/register")) { // redirect for setLogin if user is logged in
-        history.replace(`/main`);
+        history.replace(`/main/rooms`);
     } else if (!isLoggedIn && (curr_url === "/rooms" || curr_url === "/" || /^\/{1}rooms\/{1}.+$/.test(curr_url) || /^\/{1}users\/{1}.+$/.test(curr_url))) { // redirect for setLogin at rooms url if usuer is not logged in
         history.replace(`/login`)
     } 
