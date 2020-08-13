845c01b704598773970ac5700c7c27f790e0fa93
Arthur Song <arthursong14@gmail.com>
2020-08-13 16:13:36 -0700

    Refactoring setLogin

--- src/redux/dispatchActions.js
+++ src/redux/dispatchActions.js
@@ -4,24 +4,12 @@ import { BASE_URL } from '../utilities/BASE_URL';
 
 //authenticating...
 const authenticate_user = (state, history, dispatch) => { // abstracted this out because I also need in my register action
-    // const body = JSON.stringify(state)
-    // const options = {
-    //     method: "POST",
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body
-    // }
     postWithToken(`${BASE_URL}/authenticate`, state)
-    // dispatch({type: 'AUTH_REQUEST'})
-    // fetch(`${BASE_URL}/authenticate`, options)
         .then(resp => resp.json())
         .then(json => {
             // console.log("in loginUser action", json);
             if (json.user) {
                 dispatch({type: 'AUTH_SUCCESS', user: json.user.data.attributes})
-                // dispatch setchips use state.chips to display chips...
                 dispatch({type: 'SET_CHIPS', chips: json.user.data.attributes.chips })
                 localStorage.setItem("token", json.auth_token);
                 dispatch(toggleLogInPage());
@@ -34,18 +22,7 @@ const authenticate_user = (state, history, dispatch) => { // abstracted this out
 }
 
 export const authenticateViaGoogle = (email, name, history) => dispatch => {
-        // const body = JSON.stringify({ email, username: name })
-        // const options = {
-        //     method: "POST",
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body
-        // }
     postWithToken(`${BASE_URL}/auth/google`, { email, username: name })
-    // fetch(`${BASE_URL}/auth/google`, options)
-    // fetch(`${DOMAIN}/auth/google_oauth2`)
         .then(resp => resp.json())
         .then(json => {
             console.log(json);
@@ -60,34 +37,24 @@ export const authenticateViaGoogle = (email, name, history) => dispatch => {
 
 export const loginUser = (state, history) => dispatch => { authenticate_user(state, history, dispatch) }
 
-export const setLogin = history => {
-    return dispatch => {
-        const token = localStorage.getItem("token");
-        // console.log('in setlogin');
-        if (token) {
-            // dispatch({type: 'AUTH_REQUEST'});
-            fetch(`${BASE_URL}/set_login`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-                .then(resp => resp.json())
-                .then(json => {
-                    // console.log("in setLogin action", json.data);
-                    if (json.data.id) {
-                        dispatch({type: 'AUTH_SUCCESS', user: json.data.attributes })
-                        dispatch({type: 'SET_CHIPS', chips: json.data.attributes.chips })
-                        
-                        handleAuthRedirect(true, history);  // created function to control for different routes for redirects
-                    } else if (json.data.error) {
-                        dispatch({type: 'AUTH_FAIL'});
-                        handleAuthRedirect(false, history);
-                    }
-                })
-        } else {
-            handleAuthRedirect(false, history)
-        }
-    }
+export const setLogin = history => dispatch => {
+    // console.log(localStorage.getItem('token'));
+    // console.log('hello??')
+    if (!localStorage.getItem('token')) { return handleAuthRedirect(false, history)}
+    fetchWithToken(`${BASE_URL}/set_login`)
+        .then(resp => resp.json())
+        .then(json => {
+            console.log("in setLogin action", json);
+            if (json.error) {
+                dispatch({type: 'AUTH_FAIL'});
+                handleAuthRedirect(false, history);
+            } else {
+                dispatch({type: 'AUTH_SUCCESS', user: json.data.attributes })
+                dispatch({type: 'SET_CHIPS', chips: json.data.attributes.chips })
+                
+                handleAuthRedirect(true, history);  // created function to control for different routes for redirects
+            } 
+        })
 }
 
 export const logOut = (history) => dispatch => {
