fc5bcecae0df3c4a3b6dd94bd81802e0d359cf41
Arthur Song <arthursong14@gmail.com>
2020-08-16 19:39:55 -0700

    Using image URLs from amazon s3

--- src/components/Room/GameBoard.js
+++ src/components/Room/GameBoard.js
@@ -4,13 +4,22 @@ import Player from './Player';
 import pot from '../../pictures/pot_icon.webp';
 import './GameBoard.css';
 
-const importAll = r => {
-    let images = {};
-    r.keys().map((item) => { return images[item.replace('./', '')] = r(item); });
-    return images;
-}
+// const importAll = r => {
+//     let images = {};
+//     r.keys().map((item) => { return images[item.replace('./', '')] = r(item); });
+//     return images;
+// }
 
-const images = importAll(require.context('../../pictures/cards', false, /\.(png|jpe?g|svg|webp)$/));
+// const cardImages = {}
+// const imageNames = ['Tc', 'Td', 'Th', 'Ts'];
+// imageNames.forEach(name => {
+//     console.log(name);
+//     cardImages[name] = require(`https://console-poker.s3.us-east-2.amazonaws.com/cards/${name}.png`);
+// })
+// console.log(imageNames);
+// const images = importAll(require.context('../../pictures/cards', false, /\.(png|jpe?g|svg|webp)$/));
+
+// const images = importAll(require.context('https://console-poker.s3.us-east-2.amazonaws.com/cards', false, /\.(png|jpe?g|svg|webp)$/));
 
 function GameBoard(){
     const round = useSelector(state => state.game.active_round);
@@ -21,7 +30,8 @@ function GameBoard(){
         <div className="gameBoard">
             <div className="gameBoard__communityCards">
                 {round?.access_community_cards !== "" && round?.access_community_cards?.split(" ").map((c, index) => 
-                    <img key={index} className="gameBoard__card" alt={c} src={images[`${c}.webp`]}/>)}
+                    // <img key={index} className="gameBoard__card" alt={c} src={cardImages[`${c}`]}/>)}
+                    <img key={index} className="gameBoard__card" alt={c} src={`https://console-poker.s3.us-east-2.amazonaws.com/cards/${c}.png`}/>)}
             </div>
             <div className="gameBoard__pot">
                 <img width="50px" alt='pot-icon' src={pot}/>
@@ -29,7 +39,8 @@ function GameBoard(){
             </div>  
             {/* {console.log('in gameBoard seats_as_users ', seats)} */}
             {/* {seats?.map((user,index) => <Player key={index} position={index} user={user} images={images}/>)} */}
-            {game.seats_as_users?.map((user, index) => <Player key={index} position={index} user={user?.data?.attributes} images={images}/>)}
+            {/* {game.seats_as_users?.map((user, index) => <Player key={index} position={index} user={user?.data?.attributes} images={cardImages}/>)} */}
+            {game.seats_as_users?.map((user, index) => <Player key={index} position={index} user={user?.data?.attributes}/>)}
         </div>
     )
 }
--- src/components/Room/PlayerCards.js
+++ src/components/Room/PlayerCards.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './PlayerCards.css';
-
+// `https://console-poker.s3.us-east-2.amazonaws.com/cards/${c}.png`
 function PlayerCards({ user, loggedInUser, images, round }) {
     const renderCards = () => {
         if (user.cards === "" || !user.playing) {
@@ -11,11 +11,14 @@ function PlayerCards({ user, loggedInUser, images, round }) {
         } else {
             if (user.username === loggedInUser.username || (round && round.phase === 3 && round.is_playing === false)){
                 return user.cards.split(" ").map((c, index) => 
-                (<img key={index} className="playerCards__card" alt={c} src={images[`${c}.webp`]}/>))
+                // (<img key={index} className="playerCards__card" alt={c} src={images[`${c}.webp`]}/>))
+                (<img key={index} className="playerCards__card" alt={c} src={`https://console-poker.s3.us-east-2.amazonaws.com/cards/${c}.png`}/>))
             } else {
                 return <>
-                    <img className="playerCards__card" alt='facedown_card' src={images[`CARD.webp`]}/>
-                    <img className="playerCards__card" alt='facedown_card' src={images[`CARD.webp`]}/>
+                    {/* <img className="playerCards__card" alt='facedown_card' src={images[`CARD.webp`]}/>
+                    <img className="playerCards__card" alt='facedown_card' src={images[`CARD.webp`]}/> */}
+                    <img className="playerCards__card" alt='facedown_card' src={`https://console-poker.s3.us-east-2.amazonaws.com/cards/CARD.png`}/>
+                    <img className="playerCards__card" alt='facedown_card' src={`https://console-poker.s3.us-east-2.amazonaws.com/cards/CARD.png`}/>
                 </>
             } 
         }
